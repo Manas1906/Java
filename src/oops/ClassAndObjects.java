e9c4da01928a1d3a6886134ebe982a92a6ebc622
Manas <acharyamanas1906@gmail.com>
2023-02-18 22:31:46 +0530

    description added for class and objects

--- src/oops/ClassAndObjects.java
+++ src/oops/ClassAndObjects.java
@@ -1,5 +1,27 @@
 package oops;
+/*1.Class is a blueprint or a template to create object.
+2.Class is a logical template to create objects that share common properties and methods.
+3.It represents the set of properties or methods that are common to all objects of one type.
+4.A class declaration is made up of the following parts:
 
+Modifiers
+Class name
+Superclass (the name of a class’ parent, if available)
+Implemented Interfaces (if any)
+Appropriate Keywords depending on whether the class extends from a Superclass and/or implements one or more interface
+Class body within curly brackets {}
+
+access_modifier class<class_name>
+{  
+    data member;  
+    method;  
+    constructor;
+    nested class;
+    interface;
+}
+*/
+//Syntax for class declaration
 public class ClassAndObjects {
-
+//here ClassAndObjects is the class name and is also called as identifier and it should be in camel case and class is a keyword
+//whatever we write inside the class,we call it as class body/definition
 }
